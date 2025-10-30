10313f8ddb60ef81d1a786e5b1dbd0f7d0e12616
Bombalaka <yotaka.johansson@hotmail.com>
2025-10-30 21:57:58 +0100

    Refactor Kubernetes deployment script to use a variable for namespace; update Helm and kubectl commands accordingly. Add a blank line in values file for improved readability.

--- infras/deploy-k8s-docker.sh
+++ infras/deploy-k8s-docker.sh
@@ -4,26 +4,28 @@
 # Variables
 CHART_PATH="../manifests/todo-app-list"
 VALUES_FILE="values-docker-desktop.yaml"
+NAMESPACE="todo-app-list" 
 
 echo "Installing Chart..."
-helm install todo-app-list "$CHART_PATH" -f "$CHART_PATH/$VALUES_FILE"
+helm install todo-app-list "$CHART_PATH" -f "$CHART_PATH/$VALUES_FILE" --namespace "$NAMESPACE" --create-namespace
 
 echo "Show list of Helm releases..."
-helm list
+helm list -n "$NAMESPACE"
 
 echo "Getting all pods..."
-kubectl get pods -n todo-app-list
+kubectl get pods -n "$NAMESPACE"
 
 echo "Getting all services..."
-kubectl get svc -n todo-app-list
+kubectl get svc -n "$NAMESPACE"
 
 echo "Waiting for the application to be ready..."
-kubectl wait --for=condition=ready pod -l app=todo-webapp -n todo-app-list --timeout=60s
+kubectl wait --for=condition=ready pod -l app=todo-webapp -n "$NAMESPACE" --timeout=60s
 echo "Waiting for mongodb to be available..."
-kubectl wait --for=condition=ready pod -l app=mongodb -n todo-app-list --timeout=120s
+kubectl wait --for=condition=ready pod -l app=mongodb -n "$NAMESPACE" --timeout=120s
+
 
 # Get WebApp NodePort
-WEBAPP_PORT=$(kubectl get svc -n todo-app-list \
+WEBAPP_PORT=$(kubectl get svc -n "$NAMESPACE" \
   -l app.kubernetes.io/component=webapp \
   -o jsonpath='{.items[0].spec.ports[0].nodePort}' 2>/dev/null)
 
@@ -32,7 +34,7 @@ if [ -n "$WEBAPP_PORT" ]; then
 fi
 
 # Get MongoExpress NodePort
-MONGO_EXPRESS_PORT=$(kubectl get svc -n todo-app-list \
+MONGO_EXPRESS_PORT=$(kubectl get svc -n "$NAMESPACE" \
   -l app.kubernetes.io/component=mongoexpress \
   -o jsonpath='{.items[0].spec.ports[0].nodePort}' 2>/dev/null)
 
